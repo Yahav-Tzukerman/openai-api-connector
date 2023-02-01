0c33b5f1c772ba741a466f5b2c87a58a7446a248
YAHAV TZUKERMAN <YAHAVT@rafael.co.il>
2023-02-01 15:19:38 +0200

    Building the template to integrate opanai api

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { json } from "body-parser";
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState<any>("");
+  const [response, setResponse] = useState<String>("");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    fetch("http://localhost:3001/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message }),
+    })
+      .then((res) => res.json())
+      .then((data) => setResponse(data.message));
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+        <button type="submit">SUBMIT</button>
+      </form>
+      <div>{response}</div>
+    </div>
+  );
+}
+
+export default App;
